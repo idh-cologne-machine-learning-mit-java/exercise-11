887dc14de00209e2397a86b54a8dcfff33e2085c
Tholke <oehlke.thomas@gmail.com>
2021-01-30 10:28:54 +0100

    Finished Exercise 11

--- src/main/java/de/ukoeln/idh/teaching/jml/ex11/Exercise11Main.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex11/Exercise11Main.java
@@ -6,8 +6,17 @@ import java.util.List;
 
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.records.reader.impl.transform.TransformProcessRecordReader;
 import org.datavec.api.split.FileSplit;
+import org.datavec.api.transform.Transform;
+import org.datavec.api.transform.TransformProcess;
+import org.datavec.api.transform.analysis.DataAnalysis;
+import org.datavec.api.transform.analysis.columns.DoubleAnalysis;
+import org.datavec.api.transform.schema.InferredSchema;
+import org.datavec.api.transform.schema.Schema;
+import org.datavec.api.transform.transform.doubletransform.MinMaxNormalizer;
 import org.datavec.api.writable.Writable;
+import org.datavec.local.transforms.AnalyzeLocal;
 
 public class Exercise11Main {
 
@@ -16,13 +25,34 @@ public class Exercise11Main {
 		FileSplit fs = new FileSplit(new File(filename));
 		RecordReader recordReader = new CSVRecordReader(1, ',');
 		recordReader.initialize(fs);
+		
+		Schema inputDataSchema = new InferredSchema(filename).build();
+		
+		double minVBG = 0.0;
+		double maxVBG = getMax(inputDataSchema, recordReader, "VBG");
+
+		
+		Transform scaleVBG = new MinMaxNormalizer("VBG", minVBG, maxVBG, 0.0, 1.0);
+		
+		TransformProcess dataTransform = new TransformProcess.Builder(inputDataSchema)
+				.removeAllColumnsExceptFor("VBG", "VBN", "VBZ", "VBP", "VBD", "VB", "NN", "NNS")
+				.transform(scaleVBG)
+				.build();
+		
+		recordReader.reset();
+		RecordReader transformRecordReader = new TransformProcessRecordReader(recordReader, dataTransform);
 
 		while (recordReader.hasNext()) {
-			List<Writable> record = recordReader.next();
+			List<Writable> record = transformRecordReader.next();
 			System.out.println(record);
 		}
 
-		recordReader.close();
+		transformRecordReader.close();
 	}
-
+	
+	public static double getMax(Schema inputDataSchema, RecordReader recordReader, String Column) {
+		DataAnalysis analysis = AnalyzeLocal.analyze(inputDataSchema, recordReader);
+		DoubleAnalysis maxAnalysis = (DoubleAnalysis) analysis.getColumnAnalysis(Column);
+		return maxAnalysis.getMaxDouble();
+	}
 }
