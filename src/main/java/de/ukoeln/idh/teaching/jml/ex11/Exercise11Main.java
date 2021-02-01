4e18cc3c053619df6d991517eb7e7a8dae5a937b
amikoz <kozenasheva@gmail.com>
2021-02-01 20:58:34 +0100

    done

--- src/main/java/de/ukoeln/idh/teaching/jml/ex11/Exercise11Main.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex11/Exercise11Main.java
@@ -8,8 +8,20 @@ import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.split.FileSplit;
 import org.datavec.api.writable.Writable;
+import org.datavec.api.records.reader.impl.transform.TransformProcessRecordReader;
+import org.datavec.api.transform.Transform;
+import org.datavec.api.transform.TransformProcess;
+import org.datavec.api.transform.analysis.columns.DoubleAnalysis;
+import org.datavec.api.transform.schema.Schema;
+import org.datavec.api.transform.transform.doubletransform.MinMaxNormalizer;
+import org.datavec.local.transforms.AnalyzeLocal;
+
 
 public class Exercise11Main {
+
+	static String[] columns = new String[] { "LS", "TO", "VBN", "''", "WP", "UH", "VBG", "JJ", "VBZ", "--", "VBP", "NN", "DT",
+	"PRP", ":", "WP$", "NNPS", "PRP$", "WDT", "(", ")", ".", ",", "``", "$", "RB", "RBR", "RBS", "VBD", "IN",
+	"FW", "RP", "JJR", "JJS", "PDT", "MD", "VB", "WRB", "NNP", "EX", "NNS", "SYM", "CC", "CD", "POS", "class" };
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		String filename = "target/imdb-train.csv";
@@ -17,12 +29,30 @@ public class Exercise11Main {
 		RecordReader recordReader = new CSVRecordReader(1, ',');
 		recordReader.initialize(fs);
 
-		while (recordReader.hasNext()) {
-			List<Writable> record = recordReader.next();
+		Schema schema = new Schema.Builder().addColumnsDouble(columns).build();
+
+		TransformProcess maximumVBG = new TransformProcess.Builder(schema).build();
+		RecordReader ReaderFindMaximumVBG = new TransformProcessRecordReader(recordReader, maximumVBG);
+
+		DoubleAnalysis DoubleAnalysis = (DoubleAnalysis) AnalyzeLocal.analyze(schema, ReaderFindMaximumVBG).getColumnAnalysis("VBG");
+		double maxVBGValue = DoubleAnalysis.getMaxDouble();
+
+		Transform MinMaxVBG = new MinMaxNormalizer("VBG", 0.0, maxVBGValue, 0.0, 1.0);
+
+		TransformProcess tp = new TransformProcess.Builder(schema).removeAllColumnsExceptFor("VBG", "VBN", "VBZ", "VBP", "VBD", "VB", "NN", "NNS")
+				.transform(MinMaxVBG)
+				.build();
+
+		recordReader.reset();
+
+		RecordReader reader = new TransformProcessRecordReader(recordReader, tp);
+
+		while (reader.hasNext()) {
+			List<Writable> record = reader.next();
 			System.out.println(record);
 		}
 
-		recordReader.close();
+		reader.close();
 	}
 
 }
