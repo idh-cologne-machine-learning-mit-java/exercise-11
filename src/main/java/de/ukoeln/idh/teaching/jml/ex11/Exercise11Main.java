c0d00d3e654d8873088aecf3e76138cd3b809c7b
kbertho1 <k.bertholdt@web.de>
2021-02-01 23:18:01 +0100

    ex11

--- src/main/java/de/ukoeln/idh/teaching/jml/ex11/Exercise11Main.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex11/Exercise11Main.java
@@ -6,8 +6,17 @@ import java.util.List;
 
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.records.reader.impl.transform.TransformProcessRecordReader;
 import org.datavec.api.split.FileSplit;
+import org.datavec.api.transform.Transform;
+import org.datavec.api.transform.TransformProcess;
+import org.datavec.api.transform.analysis.DataAnalysis;
+import org.datavec.api.transform.analysis.columns.DoubleAnalysis;
+import org.datavec.api.transform.schema.InferredSchema;
+import org.datavec.api.transform.schema.Schema;
+import org.datavec.api.transform.transform.doubletransform.MinMaxNormalizer;
 import org.datavec.api.writable.Writable;
+import org.datavec.local.transforms.AnalyzeLocal;
 
 public class Exercise11Main {
 
@@ -16,13 +25,30 @@ public class Exercise11Main {
 		FileSplit fs = new FileSplit(new File(filename));
 		RecordReader recordReader = new CSVRecordReader(1, ',');
 		recordReader.initialize(fs);
+		
+		Schema inputSchema = new InferredSchema(filename).build();
+		double minVBG = 0.0;
+		DataAnalysis analysis = AnalyzeLocal.analyze(inputSchema,recordReader);
+		DoubleAnalysis maxAnalysis = (DoubleAnalysis) analysis.getColumnAnalysis("VBG");
+		double maxVBG = maxAnalysis.getMaxDouble();
+		
+		Transform scaleVBG = new MinMaxNormalizer("VBG", minVBG, maxVBG, 0.0, 1.0);
+		
+		//removing does not work
+		TransformProcess dataTransform = new TransformProcess.Builder(inputSchema)
+				.removeAllColumnsExceptFor("VBG", "VBN", "VBZ", "VBP", "VBD", "VB", "NN", "NNS")
+				.transform(scaleVBG)
+				.build();
+		recordReader.reset();
+		
+		RecordReader transformRecordReader = new TransformProcessRecordReader(recordReader, dataTransform);
 
 		while (recordReader.hasNext()) {
 			List<Writable> record = recordReader.next();
 			System.out.println(record);
 		}
-
-		recordReader.close();
+		
+		transformRecordReader.close();
 	}
 
 }
