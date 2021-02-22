1deefd59a7b8102dc7599d764694451bc35d0b11
Cosima Granderath-Miegel <cgrander@smail.uni-koeln.de>
2021-02-22 13:25:12 +0100

    done with the exercise

--- src/main/java/de/ukoeln/idh/teaching/jml/ex11/Exercise11Main.java
+++ src/main/java/de/ukoeln/idh/teaching/jml/ex11/Exercise11Main.java
@@ -6,8 +6,18 @@ import java.util.List;
 
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.records.reader.impl.transform.TransformProcessRecordReader;
 import org.datavec.api.split.FileSplit;
 import org.datavec.api.writable.Writable;
+import org.datavec.api.transform.TransformProcess;
+
+import org.datavec.api.transform.schema.InferredSchema;
+import org.datavec.api.transform.schema.Schema;
+import org.datavec.api.transform.transform.column.RemoveAllColumnsExceptForTransform;
+import org.datavec.api.transform.transform.doubletransform.MinMaxNormalizer;
+
+import org.datavec.api.transform.analysis.DataAnalysis;
+import org.datavec.local.transforms.AnalyzeLocal;
 
 public class Exercise11Main {
 
@@ -17,12 +27,26 @@ public class Exercise11Main {
 		RecordReader recordReader = new CSVRecordReader(1, ',');
 		recordReader.initialize(fs);
 
-		while (recordReader.hasNext()) {
-			List<Writable> record = recordReader.next();
+		Schema inputSchema = new InferredSchema(filename).build();
+
+		//In the git of datavec I found a max method, but didn't know how to call it
+		//could have been more elegant, efficient to call
+		/*DataAnalysis getMaxOfVBG = new AnalyzeLocal().analyze(inputSchema, recordReader);
+		System.out.println(getMaxOfVBG.getColumnAnalysis("VBG"));*/
+
+		TransformProcess tp = new TransformProcess.Builder(inputSchema)
+									.transform(new RemoveAllColumnsExceptForTransform("VBG","VBN","VBZ","VBP","VBD","VB","NN","NNS"))
+									.transform(new MinMaxNormalizer("VBG", 0, 54, 0, 1)).build();
+
+		RecordReader tpr = new TransformProcessRecordReader(recordReader, tp);
+
+		while (tpr.hasNext()) {
+			List<Writable> record = tpr.next();
 			System.out.println(record);
 		}
 
 		recordReader.close();
+		tpr.close();
 	}
 
 }
